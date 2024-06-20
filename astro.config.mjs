8b1f577bf85ba79203731b4061e5b1acbd671758
Gilvarsan <gilvarsan@gmail.com>
2024-06-19 20:40:56 -0500

    Se agrega el paquete auth-astro para la autenticación con Google y se implementa funcionalidad para iniciar y cerrar sesión

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
-
 import react from "@astrojs/react";
+
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), auth()],
   output: "server",
   adapter: node({
     mode: "standalone"
